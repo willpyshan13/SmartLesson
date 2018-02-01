4a4cee835884558dc9681f87c371574ffe8e9d42
will.peng <545512533@qq.com>
2018-02-01 16:37:43 +0800

    1、添加图标 2、添加课程选择

---
+++ app/src/main/java/cn/smart/smartlesson/LessonLevelPickActivity.java
@@ -0,0 +1,37 @@
+package cn.smart.smartlesson;
+
+import android.view.View;
+
+import cn.smart.baselibrary.view.BaseActivity;
+
+/**
+ * @desc: TODO
+ * @author: pengysh
+ * @createdTime: 2018-1-30 19:30
+ */
+public class LessonLevelPickActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_lesson_pick;
+    }
+
+    @Override
+    protected void performClick(View v) {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
